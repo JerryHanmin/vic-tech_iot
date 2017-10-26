8b593eddae66cfe325ac18f5848bbcee5db1337c
hanmin <jerryhanm@foxmail.com>
2017-10-26 11:56:47 +0800

    结构调整

--- oauth2-service/src/main/java/com/vic/iot/oauth2/server/repository/OauthClientsDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vic.iot.oauth2.server.repository;
-
-
-import com.vic.iot.oauth2.server.entity.OauthClientDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OauthClientsDetailsRepository extends MongoRepository<OauthClientDetails, String> {
-}
